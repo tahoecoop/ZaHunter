3639af0ceb71cd7b70cb29f5be1cc773c4a38521
tahoecoop <username@users.noreply.github.com>
2015-07-29 13:06:29 -0700

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Benjamin COOPER on 7/29/15.
+//  Copyright (c) 2015 Ben Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZaHunter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZaHunter
+//
+//  Created by Benjamin COOPER on 7/29/15.
+//  Copyright (c) 2015 Ben Cooper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
